95ef40f0eadadf10af82c1e2b313fe4ee11ceb65
AV031 <anantomi96@gmail.com>
2026-09-25 11:51:11 +0530

    222

--- Python/NLP_Lab6.py
+++ Python/NLP_Lab6.py
@@ -1,5 +1,6 @@
 # Assignment 6 
 #Q) Take a sentence, use top down parsing technique, generate indvidual tokens (Tree Structure)
+
 class Node:
     def __init__(self, name):
         self.name = name
@@ -14,30 +15,29 @@ class Node:
             child.display(level + 1)
 
 
-# Take sentence from user
+
 sentence = input("Enter a sentence: ")
 
-# Split sentence into words
+
 tokens = sentence.split()
 
-# Create root node
+
 root = Node("S")
 
-# Create NP for first part
+
 np = Node("NP")
 
-# Add words to NP
+
 for word in tokens[:-1]:
     np.add_child(Node(word))
 
-# Create VP for last word
+
 vp = Node("VP")
 vp.add_child(Node(tokens[-1]))
 
-# Add NP and VP to root
+
 root.add_child(np)
 root.add_child(vp)
 
-# Display tree
 print("\nParse Tree:")
 root.display()
